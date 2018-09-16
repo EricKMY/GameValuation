859a86f920f13f4e5464566b10df20ab8edef8b7
ivy888101 <ivy888101@gmail.com>
2018-09-16 16:08:38 +0800

    clean up text

--- DataClassification/main.py
+++ DataClassification/main.py
@@ -5,8 +5,12 @@ from module.featureDigitalize import FeatureDigitalize
 from module.dataTraining import DataTraining
 
 def main():
-    trainFeature = GameFeature("localhost", "root", "5566", "steam", "top_seller").Create()
-    trainData = FeatureDigitalize(trainFeature).Digitalize()
+    # trainFeature = GameFeature("localhost", "root", "5566", "steam", "top_seller").Create()
+    trainFeature = GameFeature("localhost", "root", "5566", "steam", "steam_spy").Create()
+    print(trainFeature)
+    # for name in trainFeature.keys():
+    #     print (trainFeature[name])
+    # trainData = FeatureDigitalize(trainFeature).Digitalize()
     # testFeature = GameFeature("localhost", "root", "5566", "steam", "top_seller_2017").Create()
     # testData = FeatureDigitalize(testFeature).Digitalize()
     # result1, result2, result3 = DataTraining(trainData, testData).TrainAndTest()
@@ -16,16 +20,16 @@ def main():
     # print('\n')
     # print(result3)
 
-    a = b = 0
-
-    for dic in trainFeature:
-        print(dic)
-        a += 1
-
-    for dic in trainData:
-        print(dic)
-        b += 1
-
-    print(a,b)
+    # a = b = 0
+    #
+    # for dic in trainFeature:
+    #     print(dic)
+    #     a += 1
+    #
+    # for dic in trainData:
+    #     print(dic)
+    #     b += 1
+    #
+    # print(a,b)
 
 print(main())
--- DataClassification/module/gameFeature.py
+++ DataClassification/module/gameFeature.py
@@ -22,50 +22,67 @@ class GameFeature():
             sell = self.CreateSell(record[2])
             price = self.CreatePrice(record[3])
             date = self.CreateDate(record[4])
+            tag = self.CreateTag(record[5])
             language = self.CreateLanguage(record[6])
-            sysReqMin = self.CreateSysReq(record[7])
-            sysReqRec = self.CreateSysReq(record[8])
 
-            if not date == "exception":
-                gameDic[name] = {'sell':sell, 'price':price, 'date':date, 'language':language, 'sysReqMin':sysReqMin, 'sysReqRec':sysReqRec}
+            # sysReqMin = self.CreateSysReq(record[7])
+            # sysReqRec = self.CreateSysReq(record[8])
+
+            # if not date == "exception":
+            gameDic[name] = {'sell':sell, 'price':price, 'date':date, 'tag':tag, 'language':language}
 
         schema.close()
         return gameDic
 
     def CreateSell(self, rawMeat):
-        if rawMeat.find('..') == -1:
-            sell = int(rawMeat)
-        else:
-            rawMeat.replace(' ','')
-            sellRange = rawMeat.split('..')
-            bottomSell = int(sellRange[0].replace(',',''))
-            topSell = int(sellRange[1].replace(',',''))
-            sell = (topSell + bottomSell) / 2
-        return sell
+        # if rawMeat.find('..') == -1:
+        #     sell = int(rawMeat)
+        # else:
+        #     rawMeat.replace(' ','')
+        #     sellRange = rawMeat.split('..')
+        #     bottomSell = int(sellRange[0].replace(',',''))
+        #     topSell = int(sellRange[1].replace(',',''))
+        #     sell = (topSell + bottomSell) / 2
+        sellDic = {}
+        sell = rawMeat.split('\xa0..\xa0')
+        sellDic['min'] = int(sell[0].replace(',', '').replace(': ', ''))
+        sellDic['max'] = int(sell[1].replace(',', ''))
+
+        return sellDic
 
     def CreatePrice(self, rawMeat):
         price = rawMeat.strip()
-        price = price.replace(' ', '')
+        price = float(price.replace('$', ''))
 
-        if price.find('Free') == -1:
-            priceList = price.split('NT$')
-            priceList.pop(0)
-            priceList = [int(element.replace(',', '')) for element in priceList]  #string 12,000 -> int 12000
-            price = min(priceList)
-        else:
-            price = 0
+        # if price.find('Free') == -1:
+        #     priceList = price.split('$')
+        #     priceList.pop(0)
+        #     priceList = [double(element.replace(',', '')) for element in priceList]  #string 12,000 -> int 12000
+        #     price = min(priceList)
+        # else:
+        #     price = 0
 
         return price
 
     def CreateDate(self, rawMeat):
         date = {}
-        dateList = rawMeat.split(', ')
-        try:
-            val = int(dateList[0].split(' ')[0])
-        except ValueError:
-            return "exception"
-        date['day'] = int(dateList[0].split(' ')[0])
-        month = dateList[0].split(' ')[-1]
+        if rawMeat.find(", ") == -1:
+            dateList = rawMeat.split(' ')
+        else:
+            dateList = rawMeat.split(', ')
+        # return dateList
+        # try:
+        #     val = int(dateList[0].split(' ')[0])
+        # except ValueError:
+        #     return "exception"
+        if len(dateList) == 4 :
+            date['day'] = 1
+            del dateList[0]
+            del dateList[-1]
+        else:
+            dateList[0] = dateList[0].replace(':', '').strip()
+            date['day'] = int(dateList[0].split(' ')[1])
+        month = dateList[0].split(' ')[0]
         monthList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
         for i in range(len(monthList)):
@@ -73,27 +90,35 @@ class GameFeature():
                 month = i + 1
 
         date['month'] = month
-        date['year']  = int(dateList[1])
+        date['year']  = int(dateList[-1])
 
         return date
 
+    def CreateTag(self, rawMeat):
+        tag = rawMeat.replace('Summary!!Atmospheric!!At!!Crafting!!Cr!!Experimental!!Ex!!Female Protagonist!!FP!!Kickstarter!!KS!!Open World!!OW!!Remake!!Re!!Space Sim!!SS!!Stealth!!St!!Warhammer 40K!!WH!!Zombies!!Zb!!', '')
+        tagList = tag.split('!!')
+        return tagList
+
     def CreateLanguage(self, rawMeat):
-        language = []
-        languageList = rawMeat.splitlines()
-        chineseExist = False
-        del languageList[0]
-
-        for element in languageList:
-            element = element.strip()
-            if element == 'Traditional Chinese' or element == 'Simplified Chinese':
-                chineseExist = True
-            else:
-                language.append(element)
-
-        if chineseExist:
-            language.append('Chinese')
-
-        return language
+        # language = []
+        # languageList = rawMeat.splitlines()
+        # chineseExist = False
+        # del languageList[0]
+        #
+        # for element in languageList:
+        #     element = element.strip()
+        #     if element == 'Traditional Chinese' or element == 'Simplified Chinese':
+        #         chineseExist = True
+        #     else:
+        #         language.append(element)
+        #
+        # if chineseExist:
+        #     language.append('Chinese')
+        #
+        # return language
+        language = rawMeat.replace('English!!EN!!Russian!!RU!!German!!DE!!Spanish!!SP!!Chinese!!CH!!French!!FR!!Polish!!PL!!Turkish!!TR!!Swedish!!SW!!', '')
+        languageList = language.split('!!')
+        return languageList
 
     def CreateSysReq(self, rawMeat):
         sysReqDic = {}
