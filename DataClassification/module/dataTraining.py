2a17cef232b0664c2a70e8d046c3f2a867511d5a
EricKuanKMY <erickuan970810edu@gmail.com>
2018-10-15 21:56:29 +0800

    finish 2.0

--- DataClassification/module/dataTraining.py
+++ DataClassification/module/dataTraining.py
@@ -13,8 +13,8 @@ class DataTraining():
         trainData = self.trainData
         testData = self.testData
 
-        train_X, train_Y, train_YMin, train_YMax = self.CreateArray(trainData)
-        test_X, test_Y, test_YMin, test_YMax = self.CreateArray(testData)
+        train_name ,train_X, train_Y, train_YMin, train_YMax = self.CreateArray(trainData)
+        test_name,test_X, test_Y, test_YMin, test_YMax = self.CreateArray(testData)
 
         module = LinearRegression()
         module.fit(train_X, train_Y)
@@ -23,31 +23,41 @@ class DataTraining():
         predictList = predictArray.tolist()
         
         resultList = []
+        smoothList = []
+        extermHighList = []
 
         for i in range(len(predictList)):
-            if predictList[i] > test_YMax[i]:
-                resultList.append((predictList[i] - test_YMax[i]) / test_YMax[i])
+            if predictList[i] > test_YMax[i]:          
+                if (predictList[i] - test_YMax[i]) / test_YMax[i] < 5:
+                    resultList.append((predictList[i] - test_YMax[i]) / test_YMax[i])
+                    smoothList.append(test_name[i])
+                else:
+                    extermHighList.append(test_name[i])
             elif predictList[i] < test_YMin[i]:
-                resultList.append((predictList[i] - test_YMin[i]) / test_YMin[i])
+                if (predictList[i] - test_YMin[i]) / test_YMin[i] >= -1:
+                    resultList.append((predictList[i] - test_YMin[i]) / test_YMin[i])
+                    smoothList.append(test_name[i])
             else:
                 resultList.append(0)
+                smoothList.append(test_name[i])
 
         result =  np.array(resultList)
         std = np.std(result, ddof = 1)
-        avg = np.histogram(result)
+        avg = np.mean(result)#np.percentile(result, [25, 50, 75])
         amin = np.amin(result)
         amax = np.amax(result)
-        plt.boxplot(result)
-        plt.show()
+        # plt.boxplot(result)
+        plt.hist(result)
+        # plt.show()
 
-        return (module.coef_, module.intercept_, avg, amin, amax, resultList)
+        return (module.coef_, module.intercept_, avg, len(resultList), len(extermHighList), resultList)
 
     def TrainAndPredict(self):
         trainData = self.trainData
         targetData = self.testData
 
-        train_X, train_Y, train_YMin, train_YMax = self.CreateArray(trainData)
-        target_X, target_Y, target_YMin, target_YMax = self.CreateArray(targetData)
+        train_name,train_X, train_Y, train_YMin, train_YMax = self.CreateArray(trainData)
+        target_name,target_X, target_Y, target_YMin, target_YMax = self.CreateArray(targetData)
 
         module = LinearRegression()
         module.fit(train_X, train_Y)
@@ -57,15 +67,17 @@ class DataTraining():
         
 
     def CreateArray(self, data):
+        data_name = []
         data_x = []
         data_y = []
         data_yMin = []
         data_yMax = []
 
         for name in data.keys():
+            data_name.append(data[name])
             data_y.append(data[name]['sell'])
             data_yMin.append(data[name]['sellMin'])
             data_yMax.append(data[name]['sellMax'])
             data_x.append([data[name]['price'], data[name]['Mlanguage'], data[name]['tag'], data[name]['introduction'], data[name]['about'], data[name]['Kview']])
 
-        return np.array(data_x), np.array(data_y), data_yMin, data_yMax
+        return data_name, np.array(data_x), np.array(data_y), data_yMin, data_yMax
--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template
-from DataClassification.module.gameFeature import GameFeature
-from DataClassification.module.featureDigitalize import FeatureDigitalize
-from DataClassification.module.dataTraining import DataTraining
+
+# from DataClassification.module.gameFeature import GameFeature
+# from DataClassification.module.featureDigitalize import FeatureDigitalize
+# from DataClassification.module.dataTraining import DataTraining
 
 app = Flask(__name__)
 
@@ -15,35 +16,22 @@ def page_a():
     sold = ''
     if request.method == 'POST':
         name = request.form.get('name', '')
-        about = request.form.get('about', '')
+        about = request.form.get('short_introduction', '')
         introduction = request.form.get('introduction', '')
         tag = request.form.get('tag', '')
         language = request.form.get('language', '')
         price = request.form.get('price', '')
-        other = request.form.get('other', '')
-        
-        # tag = self.CreateTag(tag)
-        # language = self.CreateLanguage(language)
-        targetList = [name, about, introduction, tag, language, price, other]
+        view = request.form.get('view', '')
 
+        targetList = [name, price, tag, language, introduction, about]
         #放進運算的function
-        trainFeature = GameFeature("localhost", "root", "5566", "steam", "steam_spy").Create()
-        trainData = FeatureDigitalize(trainFeature).Digitalize()
-        coef, intercept, std, amin, amax, result = DataTraining(trainData, trainData).TrainAndTest()
+        # trainFeature = GameFeature("localhost", "root", "5566", "steam", "steam_spy").Create()
+        # trainData = FeatureDigitalize(trainFeature).Digitalize()
+        # targetData = FeatureDigitalize(targetList).Digitalize()
+        # coef, intercept, std, amin, amax, result = DataTraining(trainData, targetData).TrainAndTest()
         #sold = 結果
     return render_template('a.html', sold=sold)
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
-
-# def __init__(self, rawMeat):
-#     self.rawMeat = rawMeat
-
-# def CreateTag(self):
-#     tagList = rawMeat.split(',')
-#     return tagList
-
-# def CreateLanguage(self):
-#     languageList = rawMeat.split(',')
-#     return languageList
